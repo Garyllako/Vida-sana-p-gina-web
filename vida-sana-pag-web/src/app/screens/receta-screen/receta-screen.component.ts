c177b73885d3c135cab019f24459ab7ebb75de5c
Luciano Donoso <luciano.donoso.b@mail.pucv.cl>
2022-06-06 20:43:19 -0400

    rutinas y recetas con json

--- vida-sana-pag-web/src/app/screens/receta-screen/receta-screen.component.ts
+++ vida-sana-pag-web/src/app/screens/receta-screen/receta-screen.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import recetasJson from '../../../assets/json/recetas.json';
 
+interface RECETAS {
+  id: Number;
+  title: String;
+  categoria: String;
+  description: String;
+  url: String;
+}
 @Component({
   selector: 'app-receta-screen',
   templateUrl: './receta-screen.component.html',
   styleUrls: ['./receta-screen.component.scss']
 })
 export class RecetaScreenComponent implements OnInit {
-
-  constructor() { }
+  Recetas: RECETAS[] = recetasJson;
+  constructor() { 
+    console.log(this.Recetas);
+  }
 
   ngOnInit(): void {
   }
--- vida-sana-pag-web/src/app/screens/rutina-screen/rutina-screen.component.ts
+++ vida-sana-pag-web/src/app/screens/rutina-screen/rutina-screen.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import rutinasJson from '../../../assets/json/rutinas.json';
 
+interface RUTINAS {
+  id: Number;
+  title: String;
+  categoria: String;
+  description: String;
+  url: String;
+}
 @Component({
   selector: 'app-rutina-screen',
   templateUrl: './rutina-screen.component.html',
   styleUrls: ['./rutina-screen.component.scss']
 })
 export class RutinaScreenComponent implements OnInit {
-
-  constructor() { }
+  Rutinas: RUTINAS[] = rutinasJson;
+  constructor() {
+    console.log(this.Rutinas);
+   }
 
   ngOnInit(): void {
   }
